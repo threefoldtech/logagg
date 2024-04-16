cb91198aa064dd2509e078503c8b194c4950092b
AbdelrahmanElawady <abdoelawady125@gmail.com>
2024-04-16 16:54:29 +0200

    Remove detach flag

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,6 @@ async fn main() -> Result<()> {
 #[derive(StructOpt, Debug)]
 #[structopt(name = "logagg")]
 pub struct CmdArgs {
-    /// Activate detach mode
-    #[structopt(short, long)]
-    pub detach: bool,
-
     /// Config file
     #[structopt(short, long, parse(from_os_str))]
     pub cfg: PathBuf,
